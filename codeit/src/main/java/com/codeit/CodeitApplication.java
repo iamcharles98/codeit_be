3d35a2d1523978820b81394bcce1dfa02589974e
chansoo <chansoo38@gmail.com>
2025-01-07 18:11:43 +0900

    project init

---
+++ codeit/src/main/java/com/codeit/CodeitApplication.java
@@ -0,0 +1,13 @@
+package com.codeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeitApplication.class, args);
+    }
+
+}
---
+++ codeit/src/test/java/com/codeit/CodeitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
