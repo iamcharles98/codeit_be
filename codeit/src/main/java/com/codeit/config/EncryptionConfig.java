015652ac1093207bb2ae622678edf08962005771
chansoo <chansoo38@gmail.com>
2025-01-18 17:05:41 +0900

    chore : 의존성 추가(Validator, PasswordEncoder)
    feat :  PasswordEncoder 빈 생성

---
+++ codeit/src/main/java/com/codeit/config/EncryptionConfig.java
@@ -0,0 +1,15 @@
+package com.codeit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class EncryptionConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
